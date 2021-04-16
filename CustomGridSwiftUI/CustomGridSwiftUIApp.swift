5b422d21820bd6875cf0372ed2ff71b7f23c36f3
Aleksei <suslov.aleksey.oo7@gmail.com>
2021-04-16 23:52:12 +0300

    Initial Commit

---
+++ CustomGridSwiftUI/CustomGridSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomGridSwiftUIApp.swift
+//  CustomGridSwiftUI
+//
+//  Created by ALEKSEY SUSLOV on 16.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct CustomGridSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
